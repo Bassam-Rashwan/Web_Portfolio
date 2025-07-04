d24d34129d7ead63945d88678b230b857f70092c
Bassam-Rashwan <bassam.ahmed.rashwan@gmail.com>
2025-07-04 19:10:12 +0300

    chore: update homepage and prepare for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/Web_Portfolio/', // Set this to your repo name
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
+    mode === 'developmenht' &&
     componentTagger(),
   ].filter(Boolean),
   resolve: {
